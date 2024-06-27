4827e7dfe894fca3a3f05692bd20f70b7b948f4e
Mamatyari700 <pandapool.1991.3@gmail.com>
2024-06-27 12:21:08 -0700

    finished clock page

--- clock.py
+++ clock.py
@@ -1,57 +1,60 @@
 import tkinter as tk
 import time
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 
-def updateTime():
-      
-	currentTime = time.strftime("%H:%M") 
-	currentSecond = time.strftime("%S")
-	dayName = time.strftime("%a")  #%A for Full day name (e.g., "Monday"), %a for short name(e.g., "Mon")
-	today = time.strftime("%Y/%m/%d")  #Date in yyyy-mm-dd format
+windowWidth = 1024		#window size of the smart display
+windowHeight = 600			
 
-	dateLabel.config(text = f"{today} | {dayName}")
-	timeLabel.config(text = currentTime)
-	secondLabel.config(text = currentSecond)
-    
-	root.after(1000, updateTime)
+x = windowWidth / 2 	#the middle point of the window
+y = windowHeight / 2
 
+image = Image.new("RGBA", (windowWidth, windowHeight), (255, 255, 255, 0))
+draw = ImageDraw.Draw(image)
 
-windowWidth = 1024
-windowHeight = 600
+fontPath = "./font/CaviarDreams_BoldItalic.ttf"		#Path of the .ttf file
+
+EightBitDragonDate = ImageFont.truetype(fontPath, 70)
+EightBitDragonTime = ImageFont.truetype(fontPath, 270)
+EightBitDragonSecond = ImageFont.truetype(fontPath, 70)
 
 root = tk.Tk()
 root.title("ClockPage") 
 root.geometry(f"{windowWidth}x{windowHeight}")	#setting windowsize for testing 
 #root.attributes("-fullscreen", True)	#make the soft fullscreen
+root.configure(bg="black")
 canvas = tk.Canvas(root, width = windowWidth, height = 2)
 canvas.create_line(0, 0, windowWidth, 0, fill = "#600000")
 
-fontPath = "../font/CaviarDreams_Bold.ttf"
+clockLabel = tk.Label(root)
 
-dateLabel = tk.Label(
-    root,
-    foreground = "#600000",
-    font = ImageFont.truetype(fontPath, 40),
-    pady = 50
-)
+clockLabel.pack()
 
-timeLabel = tk.Label(
-    root,
-    foreground = "#600000",
-    font = ImageFont.truetype(fontPath, 250),
-    pady = 0
-)
+def drawClock(dayName, today, currentTime, currentSecond):
+    image = Image.new("RGBA", (windowWidth,windowHeight), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+   
+    draw.text((170, 70), f"{today} | {dayName}", font = EightBitDragonDate, fill = "#600000") #text(50,50) means the left upper corner is 50 pixcel down, 50 pixel left form bace picture's left upper corner
+    draw.text((50, 230), f"{currentTime}", font = EightBitDragonTime, fill = "#600000")
+    draw.text((900, 410), f"{currentSecond}", font = EightBitDragonSecond, fill = "#600000")
+    
+    return ImageTk.PhotoImage(image)
 
-secondLabel = tk.Label(
-    root,
-    foreground = "#600000",
-    font = ImageFont.truetype(fontPath, 50)
-)
-
-dateLabel.pack()
-canvas.pack()
-timeLabel.place(relx = 0.5, rely = 0.5, anchor = tk.CENTER)
-secondLabel.place(relx = 0.9, rely = 0.725, anchor = tk.SW) #increase x => going rigiht, increase y => going down
+def updateTime():
+    draw.rectangle((0, 0, windowWidth, windowHeight), fill = (255, 255, 255, 0))
+    
+    dayName = time.strftime("%a")  #%A for Full day name (e.g., "Monday"), %a for short name(e.g., "Mon")
+    today = time.strftime("%Y/%m/%d")  #Date in yyyy-mm-dd format
+    currentTime = time.strftime("%H:%M")
+    currentSecond = time.strftime("%S")
+    
+    clockImage = drawClock(dayName, today, currentTime, currentSecond)
+    
+    # Update labels with new images 
+    clockLabel.config(image = clockImage)
+    clockLabel.image = clockImage
+    
+    # Schedule the next update
+    root.after(1000, updateTime)
 
 #def close_window(event = None):
 #    root.attributes('-fullscreen', False)  
