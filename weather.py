8c042309a1961e3efe73e6b4c269d533e470b447
Mamatyari700 <pandapool.1991.3@gmail.com>
2024-06-28 00:37:14 -0700

    picked up all info that need for current weather data, deleted ipify(weather API already have it)

--- weather.py
+++ weather.py
@@ -1,39 +1,36 @@
 #http://api.weatherapi.com/v1 #base url for weather API
-#API key = c526e7caac4c403e9f4212109242706
-#API key for ipify = https://geo.ipify.org/api/v2/country,city?apiKey=at_uJGnmiXcuETNuyc3tukfxtMY75Iu9
+#API key for weatherAPI= c526e7caac4c403e9f4212109242706
 
 #import tkinter as tk
 #import time
 #from PIL import Image, ImageDraw, ImageFont, ImageTk
 import requests
 
-ipifyURL = "https://geo.ipify.org/api/v2/country,city?apiKey=at_uJGnmiXcuETNuyc3tukfxtMY75Iu9"
-
-response = requests.get(ipifyURL)
-
-data = response.json()
-
-print(f"IPアドレス: {data['ip']}")
-print(f"場所: {data['location']['city']}, {data['location']['region']}, {data['location']['country']}")
-print(f"緯度: {data['location']['lat']}, 経度: {data['location']['lng']}")
-
 APIKeyForWeatherAPI = "c526e7caac4c403e9f4212109242706"
-longitude = data['location']['lat']
-latitude = data['location']['lng']
+#longitude = data['location']['lat']
+#latitude = data['location']['lng']
 
-def getWeatherData(APIKey, longitude, latitude):
-    weatherAPIUPL = f"http://api.weatherapi.com/v1/current.json?key={APIKey}&q={longitude},{latitude}"
+def getCurrentWeatherData(APIKey):
+    weatherAPIUPL = f"http://api.weatherapi.com/v1/current.json?key={APIKey}&q=auto:ip"
     response = requests.get(weatherAPIUPL)
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-weatherData = getWeatherData(APIKeyForWeatherAPI, longitude, latitude)
+weatherData = getCurrentWeatherData(APIKeyForWeatherAPI)
 
-print(f"現在の天気: {weatherData['current']['condition']['text']}")
+print(f"現在地: {weatherData['location']['name']}, {weatherData['location']['region']}, {weatherData['location']['country']}")
+print(f"現在時刻: {weatherData['location']['localtime']}")
+print(f"現在の天気: {weatherData['current']['condition']['icon']}")
 print(f"気温: {weatherData['current']['temp_c']} ℃")
 print(f"湿度: {weatherData['current']['humidity']} %")
+print(f"風速: {weatherData['current']['wind_kph']} km/h")
+print(f"気圧: {weatherData['current']['pressure_mb']} hPa")
+print(f"体感温度: {weatherData['current']['feelslike_c']} ℃")
+
+#print(latAndLon)
+#print(weatherData)
 
 
     
